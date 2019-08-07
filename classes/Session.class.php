0b48def4dfe172fdfa8b073b9668ffc6fff9949a
anouckdeben95 <r0456343@student.thomasmore.be>
2019-08-07 14:14:18 +0200

    Create Session.class.php
    sessie creeëren en checken

---
+++ classes/Session.class.php
@@ -0,0 +1,23 @@
+
+<?php
+    Abstract class Session{
+        private static function start(){
+            return session_start();
+        }
+        public static function create($id){
+            self::start();
+            $_SESSION["userID"] = $id;
+        }
+        public static function check(){
+            self::start();
+            if(!empty($_SESSION["userID"])){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        public static function destroy(){
+            return session_destroy();
+        }
+    }
