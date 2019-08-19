4692e05a6ce36a1d84e37b93ac52c38b30a4fc4c
anouckdeben95 <r0456343@student.thomasmore.be>
2019-08-19 21:41:50 +0200

    Update Task.class.php
    order tasks correctly:
    first: not done & date-wise
    second: done
    
    add function done to update database when task is done

--- classes/Task.class.php
+++ classes/Task.class.php
@@ -7,11 +7,15 @@
 
         public function getTasks($listItemId){
             $conn = Db::getInstance();
-            $sql = "SELECT tl_tasks.id, tl_tasks.name, tl_tasks.duration, tl_tasks.deadline 
+            //https://stackoverflow.com/questions/46339436/sql-order-by-0-first-and-then-descend-number-values/46339453
+            $sql = "SELECT tl_tasks.id, tl_tasks.name, tl_tasks.duration, tl_tasks.deadline, tl_tasks.done 
                     FROM tl_tasks 
                     INNER JOIN tl_lists 
                     ON tl_tasks.list_id = tl_lists.id
-                    WHERE tl_lists.id = :taskid";
+                    WHERE tl_lists.id = :taskid
+                    ORDER BY 
+                    case when tl_tasks.done = 0 then 0
+                    else 1 end, tl_tasks.deadline ASC";
             $statement = $conn->prepare($sql);
             $statement->bindValue(':taskid', $listItemId);
             $statement->execute();
@@ -36,6 +40,15 @@
            
         }
 
-
+        public function done($id){
+            $conn = Db::getInstance();
+            $sql = "UPDATE tl_tasks
+                    SET done = 1
+                    WHERE tl_tasks.id = :id";
+            $statement = $conn->prepare($sql);
+            $statement->bindValue(':id', $id);
+            $result = $statement->execute();
+            return $result;         
+        }
 
     }
