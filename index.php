ee7d74726872fe0cb69e54274dc2b16cb9ed5882
anouckdeben95 <r0456343@student.thomasmore.be>
2019-08-21 18:37:30 +0200

    Update index.php
    check if user is admin to choose the right nav

--- index.php
+++ index.php
@@ -7,12 +7,10 @@
     }
     else{
         $id = $_SESSION['userID'];
-
         $l = new TodoList();
         $results = $l->getLists($id);
     }
 
-    
     
 ?><!DOCTYPE html>
 <html lang="en">
@@ -66,7 +64,16 @@
 </head>
 <body>
     <header>
-        <?php require_once("nav.inc.php"); ?>
+        <?php 
+            $id = $_SESSION['userID'];
+            $a = new Admin();
+            $isAdmin = $a->checkAdmin($id);
+            if ($isAdmin){
+                require_once("nav2.inc.php");
+            } else {
+                require_once("nav.inc.php");
+            }
+         ?>
         <h1>Todo App</h1>
     </header>
     <main>
