755c84c2b86266aac61a186b51cf84a96c5b26d2
anouckdeben95 <r0456343@student.thomasmore.be>
2019-08-21 18:38:04 +0200

    Create admin.php
    make admin page, where they can remove and add admins

---
+++ admin.php
@@ -0,0 +1,82 @@
+<?php
+    require_once("bootstrap.php");
+
+    $s = Session::check();
+    if($s === false){
+        header("Location: login.php");
+    }
+    else{
+        $id = $_SESSION['userID'];
+        $u = new User();
+        $results = $u->getUsers();
+    }
+
+    
+    
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel = "stylesheet" type = "text/css" href = "css/reset.css"/>
+    <link rel = "stylesheet" type = "text/css" href = "css/style.css"/>
+    <link rel = "stylesheet" type = "text/css" href = "css/add.css"/>
+
+    <title>TodoApp - admin</title>
+
+</head>
+<body>
+    <header>
+        <?php require_once("nav2.inc.php"); ?>
+        <h1>Admin</h1>
+    </header>
+    <main>
+        <div class="whitelist">
+        <h1>Users</h1>
+        <p>Manage the admins</p>
+        <table class="listing">
+        <thead>
+        <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Username</th>
+        <th>Admin<th>
+        <th></th>
+        <th></th>
+        </tr>
+        </thead>
+        
+        <tbody>
+        <?php
+        foreach($results as $row)
+        {
+            if($row['isAdmin']){
+                $ad = "Yes";
+            } else {
+                $ad ="No";
+            }
+            echo "<tr>";
+            echo "<td>" . $row['firstname'] . "</td>";
+            echo "<td>" . $row['lastname'] . "</td>";
+            echo "<td>" . $row['username'] . "</td>";
+            echo "<td>" . $ad . "</td>";
+            echo "<td><input type='submit' id='".$row['id']."' class='btn addbtn' value='ADD'></td>";
+            echo "<td><input type='submit' id='".$row['id']."' class='btn rembtn' value='REMOVE'></td>";
+            echo "</tr>";
+        }
+        ?>
+        </tbody>
+        </table>
+
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="js/admin.js"></script>
+
+</html>
