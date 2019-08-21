4d0752f7133f6ef6ad23cfee84649c1436340482
anouckdeben95 <r0456343@student.thomasmore.be>
2019-08-21 23:59:26 +0200

    Update tasklist.php

--- tasklist.php
+++ tasklist.php
@@ -4,7 +4,7 @@
     $s = Session::check();
     if($s === false){
         header("Location: login.php");
-    } else {
+    } else {    
         $listItemId = $_GET['id'];
 
         $t = new Task;   
@@ -16,6 +16,30 @@
         $comments = $c->getComments($taskid);
         return $comments;
     }
+
+    //upload image to DB
+    //https://justclack.blogspot.com/2017/08/image-upload-in-php-pdo-with-database.html
+    if(isset($_POST['uploadbtn'])) { 
+
+        $newImage = $_FILES['image'];
+        $rowidimage = $_POST['rowid'];
+
+        $image = new Image();
+        if ($image->checkType($newImage) === false) {
+            $feedback = 'Sorry, only JPG, JPEG, PNG & PDF files are allowed.';
+        } else {
+            $image->createDirectory('images');
+            if ($image->fileExists() === false) {
+                $feedback = 'Sorry, this file already exists. Please try again.';
+            } else {
+                $image->insertIntoDB($image->uploadImage(), $rowidimage);
+                $feedback = 'File has been uploaded.';
+                header("Location: tasklist.php?id=".$listItemId);
+            }
+                            
+        } 
+    }  
+  
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -38,8 +62,24 @@
         table{
             border-collapse: collapse;
         }
-        .listing tbody tr:nth-child(odd){
+        .listing tbody .mainrow{
             border-bottom: 5px solid #4D5061;
+        }
+        .upload{
+            display: none; 
+            position: fixed;
+            z-index: 1; /* Sit on top */
+            padding-top: 100px; 
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%; 
+            background-color: rgb(0,0,0); /* Fallback color */
+            background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+        }
+        .image{
+            width: 200px;
+            height: auto;
         }
     </style>
 
@@ -75,6 +115,7 @@
         <th>Duration (min)</th>
         <th class='dl'>Deadline</th>
         <th></th>
+        <th></th>
         </tr>
         </thead>
 
@@ -102,9 +143,20 @@
             <a class='pen' onclick='openPopup(". $row['id'] .")'><img src='images/edit_icon_s.png'></a></td>";
             echo "<td><a onclick='openComments(". $row['id'] .")'>
             <img src='images/add_icon_s.png'></a></td>";
+            echo "<td><a onclick='openUpload(". $row['id'] .")'>
+            <img src='images/addimage_icon_s.png'></a></td>";
             echo "</tr>";
 
-            echo "<tr id='commentbtn". $row['id'] ."' class='commentbtn'><td colspan='4'>";
+            $image = new Image;
+            $i = $image->getImage($row['id']);
+            foreach($i as $img){
+            echo "<tr class='mainrow ". $k ."'>
+            <td colspan='4'><img class='image' src='". $img['image'] ."'></td>";?>
+            <?php echo "<td colspan='2'><input style='width: 30px;' name='imgrembtn' class='btn imgbtn' value='DEL' id=". $row['id'] ."></td>
+            </tr>";
+            }
+
+            echo "<tr id='commentbtn". $row['id'] ."' class='commentbtn'><td colspan='6'>";
             ?>
                 <ul id="listupdates" class="updates">
                     <?php 
@@ -146,6 +198,24 @@
 
                                 <input class="update_task" type="submit" value="Update" id="<?php echo $row['id'] ?>">
                             </div>
+                        </form>
+                    </div>
+                </div>
+
+            <!-- Upload popup -->
+                <div id="upload<?php echo $row['id'] ?>" class="upload">
+                    <div class="popup">
+                        <?php echo "<span onclick='closing2(". $row['id'] .")' class='close' >&times;</span>"; ?>
+                        <form action="" method="post" class="update" enctype="multipart/form-data">
+                        <?php if(isset($feedback)){
+                            echo "<div>
+                            <p>". $feedback ."</p>
+                            </div>";
+                        } ?>
+                            <input type="file" name="image" id="upFile" accept=".png,.gif,.jpg,.webp" required><br>
+                            <input style='display: none;' type="text" name="rowid" value="<?php echo $row['id'] ?>">
+                            <input type="submit" name="uploadbtn" value="Upload Image" id=""> 
+                        
                         </form>
                     </div>
                 </div>
@@ -193,6 +263,22 @@
         var modal = document.getElementById(id);
         modal.style.display = "none";
     }
+
+    function openUpload(rowid) {
+        var id = "upload".concat(rowid);
+        
+        console.log(id);
+        // Get the modal
+        var upl = document.getElementById(id);
+        upl.style.display = "block";
+    }
+
+    function closing2(rowid) {
+        var id = "upload".concat(rowid);
+        console.log(id);
+        var upl = document.getElementById(id);
+        upl.style.display = "none";
+    }
    
 
 
@@ -205,4 +291,5 @@
 <script src="js/comment.js"></script>
 <script src="js/checked.js"></script>
 <script src="js/update.js"></script>
+<script src="js/delete.js"></script>
 </html>
